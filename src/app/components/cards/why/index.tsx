1c21b14e64d9b64fb5bc892b05c895d900bf996f
maxidefilippis <maxidefilippis@gmail.com>
2024-03-31 22:08:34 -0300

    created whyCard component

---
+++ src/app/components/cards/why/index.tsx
@@ -0,0 +1,31 @@
+import { TitleType } from '@/app/constants/text';
+import Image from 'next/image';
+import { Text } from '../../text';
+import { Title } from '../../title';
+import styles from './index.module.css';
+
+interface WhyCardProps {
+    icon?: any;
+    title: string;
+    description: string;
+}
+export const WhyCard = ({ icon, title, description }: WhyCardProps) => {
+    return (
+        <article className={styles.whyCard}>
+            <Image
+                src={icon}
+                alt={title}
+                className={styles.whyCardIcon}
+            />
+            <Title
+                type={TitleType.H3}
+                text={title}
+                classes={styles.whyCardTitle}
+            />
+            <Text
+                text={description}
+                classes={styles.whyCardDescription}
+            />
+        </article>
+    );
+};
