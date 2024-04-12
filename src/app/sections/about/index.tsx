fe536c0e50eb6859eff532d52b76aa7a479e8bbd
maxidefilippis <maxidefilippis@gmail.com>
2024-04-12 16:37:53 -0300

    updated images format

--- src/app/sections/about/index.tsx
+++ src/app/sections/about/index.tsx
@@ -3,7 +3,7 @@ import { Text } from '@/app/components/text';
 import styles from './index.module.css';
 import Image from 'next/image';
 import { TitleType } from '@/app/constants/text';
-import employees from '/public/images/employees.webp';
+import employees from '/public/images/employees.png';
 
 export const AboutSection = () => {
     return (
